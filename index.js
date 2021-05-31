a8b089a9ae505d91d392ad1b66e0e28a4a634632
JunSeong <ebseud6135@naver.com>
2021-05-31 17:51:56 +0900

    feat: 좋아요, 싫어요 기능 추가

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const bgm = new Audio("the-girl-who-leapt-through-time-ost.mp3");
 bgm.loop = true;
 
-const bgmPlayButton = document.getElementById("bgm-play-button");
-const bgmStopButton = document.getElementById("bgm-stop-button");
+const $bgmPlayButton = document.getElementById("bgm-play-button");
+const $bgmStopButton = document.getElementById("bgm-stop-button");
 
-bgmPlayButton.addEventListener("click", function () {
+$bgmPlayButton.addEventListener("click",  () => {
   bgm.play();
-  bgmPlayButton.style.display = "none";
-  bgmStopButton.style.display = "block";
+  $bgmPlayButton.style.display = "none";
+  $bgmStopButton.style.display = "block";
 });
 
-bgmStopButton.addEventListener("click", function () {
+$bgmStopButton.addEventListener("click",  () => {
   bgm.pause();
-  bgmStopButton.style.display = "none";
-  bgmPlayButton.style.display = "block";
+  $bgmStopButton.style.display = "none";
+  $bgmPlayButton.style.display = "block";
 });
+
+const likeKey = "like-count";
+const disLikeKey = "dis-like-count";
+let $likeTable = document.getElementById("like-table");
+let $likeImage = $likeTable.getElementsByTagName("img")[0];
+let $likeCount = $likeTable.getElementsByTagName("h5")[0];
+let $disLikeTable = document.getElementById("dis-like-table");
+let $disLikeImage = $disLikeTable.getElementsByTagName("img")[0];
+let $disLikeCount = $disLikeTable.getElementsByTagName("h5")[0];
+
+
+initCount(likeKey, $likeCount);
+initCount(disLikeKey, $disLikeCount);
+addEventIncreaseCount(likeKey, $likeImage, $likeCount);
+addEventIncreaseCount(disLikeKey, $disLikeImage, $disLikeCount);
+
+function initCount(localStorageKey, $count) {
+  let item = localStorage.getItem(localStorageKey);
+  if (!item) {
+    $count.innerText = "7"
+    localStorage.setItem(localStorageKey, "7");
+  } else {
+    $count.innerText = item;
+  }
+}
+
+function addEventIncreaseCount(localStorageKey, $image, $count) {
+  $image.addEventListener("click", () => {
+    let increasedCount = eval($count.innerText + "+ 1");
+    $count.innerText = increasedCount;
+    localStorage.setItem(localStorageKey, increasedCount);
+  });
+}
