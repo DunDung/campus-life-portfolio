8d7145ce7d8934b90454c39fcea38db9af349c73
JunSeong <ebseud6135@naver.com>
2021-06-11 17:26:31 +0900

    feat: 시계 기능 추가

--- index.js
+++ index.js
@@ -120,3 +120,22 @@ function sendMail(nickname, comment) {
     .send("service_2syktss", "template_4nk0rnw", templateParams)
     .then(() => window.location.reload());
 }
+let $time = document.getElementById("time");
+let $date = document.getElementById("date");
+
+function getTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  $date.innerText = year + "-" + month + "-" + date;
+
+  let hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+  let amOrPm = (hour < 12) ? "오전" : "오후";
+  hour = (hour === 12) ? hour : hour % 12;
+  $time.innerText = amOrPm + " " + hour + ":" + minute + ":" + second;
+}
+setInterval(getTime, 1000);
+
